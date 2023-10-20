a3011634da73b7300ab4a68bce66365a66ab8eaf
iH0DL <73050632+i-hodl@users.noreply.github.com>
2023-10-20 13:18:44 -0400

    A python script to reclaim your data and pin....

---
+++ HEADERS.py
@@ -0,0 +1,8 @@
+from RARIBLE_API_KEY import RARIBLE_API_KEY
+
+
+HEADERS = {
+    'Authorization': f'Bearer {RARIBLE_API_KEY}',
+    'Accept': 'application/json',
+    'X-API-KEY': RARIBLE_API_KEY
+}
